c76d1380e74f8cd53a9b51ee17bd5e98b683fe65
jerryzlei <784378493@qq.com>
2018-10-30 22:21:06 +0800

    配置更改

---
+++ news-common/src/main/java/com/jerry/news/common/encryption/Algorithm.java
@@ -0,0 +1,11 @@
+package com.jerry.news.common.encryption;
+
+/**
+ * @Author： zhanlei
+ * @Description：
+ * @CreateTime: 12:51 2018/10/30
+ * @Modified by:
+ */
+public class Algorithm {
+    public static final String SHA = "sha";
+}
